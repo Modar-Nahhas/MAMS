dcbd9d917f61057c98a4ee1db46b64f4efe6553d
Tatweer <modar.nahhas@tatweer.sy>
2023-11-24 19:03:19 +0300

    Prepare exception handling and json response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public static function getJsonResponse($message, $data = null, $result = true, $code = 200): JsonResponse
+    {
+        $response = [
+            'message' => $message,
+            'data' => $data,
+            'status' => $result,
+            'code' => $code,
+        ];
+        return response()->json($response, $code);
+    }
 }
